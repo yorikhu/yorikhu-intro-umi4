66e0f417e180af7472241b27bd9d909d80d331b3
yorikhu <13003682137@163.com>
2022-07-29 17:55:41 +0800

    feat: add auth

--- src/access.ts
+++ src/access.ts
@@ -1,8 +1,24 @@
-export default (initialState: API.UserInfo) => {
-  const canSeeAdmin = !!(
-    initialState && initialState.name !== 'dontHaveAccess'
-  );
+import { AuthType } from './constants';
+
+export default (initialState: User.LoginResult) => {
+  const authLicense = {
+    canSeeAdmin: false,
+    canSeeManager: false,
+    canSeeUser: false,
+    canSeeTourist: true,
+  };
+  switch (initialState.auth) {
+    case AuthType.Admin:
+      authLicense.canSeeAdmin = true;
+    case AuthType.Manager:
+      authLicense.canSeeManager = true;
+    case AuthType.User:
+      authLicense.canSeeUser = true;
+
+    default:
+      console.warn('您还没有登陆哦! ฅʕ•̫͡•ʔฅ');
+  }
   return {
-    canSeeAdmin,
+    ...authLicense,
   };
 };
