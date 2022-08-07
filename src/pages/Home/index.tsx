90ce4b532e08b7c08e3c9d46afe01c2e57a5e415
inkroll <13003682137@163.com>
2022-08-07 23:08:57 +0800

    feat: owner's inroduction in the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
+import { OWNER_NICK_CH_NAME, OWNER_NICK_EN_NAME } from '@/constants';
 import styles from './index.less';
 
 const HomePage: React.FC = () => {
   return (
     <div className={styles.home}>
-      <header></header>
-      <main>{"page's content"}</main>
+      <main>
+        <div className={styles['owner-desc']}>
+          <div className={styles['avatar']}>
+            <img src={require('@/assets/avatar.jpg')} />
+          </div>
+          <div className={styles['nickname']}>
+            <p className={styles['ch-name']}>{OWNER_NICK_CH_NAME}</p>
+            <p className={styles['en-name']}>{OWNER_NICK_EN_NAME}</p>
+          </div>
+          <div className={styles['bubbles-list']}>
+            <div className={styles['bubble']} />
+            <div className={styles['bubble']} />
+            <div className={styles['bubble']} />
+            <div className={styles['bubble']} />
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
