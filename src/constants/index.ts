72dd72ebbcc419b5b405fb04a05181778fa11bdc
yorikhu <13003682137@163.com>
2022-08-07 23:08:57 +0800

    feat: owner's inroduction in the homepage

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
-export const DEFAULT_NAME = 'Inkroll';
+export const OWNER_NICK_EN_NAME = 'inkroll';
+export const OWNER_NICK_CH_NAME = '墨卷';
+export const OWNER_AVATAR = '@/assets/avatar.jpg';
 
 /** 用户角色类型 */
 export enum AuthType {
